04fb8b2bff899e86365b6f71329419735888c526
ShobhitWK <shobhit@webkorps.com>
2022-07-24 00:42:01 +0530

    added ENDER in worker

--- worker.rb
+++ worker.rb
@@ -1,7 +1,11 @@
+require_relative "final.rb"
+
 module Calculate
+
     class Calculator
         def initialize(equation)
             solved_equation = eval(equation)
+            puts ENDER
             puts equation.to_s + " = " + solved_equation.to_s
         end
     end
