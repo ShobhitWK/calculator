f56ee15874d167a18e093aa241c761f875f3ac78
ShobhitWK <shobhit@webkorps.com>
2022-07-24 00:24:59 +0530

    Converted the whole code into mixin

--- calculator-task.rb
+++ calculator-task.rb
@@ -1,45 +1,5 @@
-MATH_REGEX = /\d+? *?\+ *?\d+?/
+require_relative "modules.rb"
 
-STARTER = "==================================Calculator=================================="
-ENDER = "=============================================================================="
-
-class Calculator
-    def initialize(equation)
-        solved_equation = eval(equation)
-        puts equation.to_s + " = " + solved_equation.to_s
-    end
-end
-
-class InputValidator
-
-    def check
-        @check = Hash.new
-        print "Enter the equation (only mathematical equation is supported): "
-        user_input = gets.chomp.to_s
-        if user_input =~ MATH_REGEX
-            @check["validated"]=true
-            @check["equation"]=user_input
-        end
-        return @check
-    end
-end
-
-while true
-    puts STARTER
-    print "Do you want to use calculator [y/n]: "
-    res = gets.chomp.to_s
-
-    if res == 'y' or res == 'Y'
-        validate = InputValidator.new
-        user_input = validate.check
-        if user_input["validated"] == true
-            Calculator.new(user_input["equation"])
-        else
-            puts "Enter a valid mathematical equation!"
-        end
-    else
-        puts "Thanks for using Calculator"
-        puts ENDER
-        break
-    end
-end
+include Calculate
+include InputValidate
+include InfinityLoop
---
+++ modules.rb
@@ -0,0 +1,54 @@
+
+STARTER = "==================================Calculator=================================="
+ENDER = "=============================================================================="
+MATH_REGEX = /\d+? *?\+ *?\d+?/
+
+module InputValidate
+    class InputValidator
+
+        def check
+            @check = Hash.new
+            print "Enter the equation (only mathematical equation is supported): "
+            user_input = gets.chomp.to_s
+            if user_input =~ MATH_REGEX
+                @check["validated"]=true
+                @check["equation"]=user_input
+            end
+            return @check
+        end
+    end
+
+end
+
+module Calculate
+    class Calculator
+        def initialize(equation)
+            solved_equation = eval(equation)
+            puts equation.to_s + " = " + solved_equation.to_s
+        end
+    end
+end
+
+module InfinityLoop
+    include InputValidate
+    include Calculate
+    while true
+        puts STARTER
+        print "Do you want to use calculator [y/n]: "
+        res = gets.chomp.to_s
+
+        if res == 'y' or res == 'Y'
+            validate = InputValidator.new
+            user_input = validate.check
+            if user_input["validated"] == true
+                Calculator.new(user_input["equation"])
+            else
+                puts "Enter a valid mathematical equation!"
+            end
+        else
+            puts "Thanks for using Calculator"
+            puts ENDER
+            break
+        end
+    end
+end
