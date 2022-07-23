3864201286dbd59b6974beeda3f3dcf4b642533d
ShobhitWK <shobhit@webkorps.com>
2022-07-24 00:52:36 +0530

    removed begin rescue as its not working

--- calculator-task.rb
+++ calculator-task.rb
@@ -1,11 +1,5 @@
 require_relative "final.rb"
 
-begin
-    include Calculate
-    include InputValidate
-    include InfinityLoop
-rescue
-    include Calculate
-    include InputValidate
-    include InfinityLoop
-end
+include Calculate
+include InputValidate
+include InfinityLoop
