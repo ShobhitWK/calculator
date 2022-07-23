48ffcb768c904821a4d8574f36dc8539d19e8747
ShobhitWK <shobhit@webkorps.com>
2022-07-24 00:56:49 +0530

    fixed

--- worker.rb
+++ worker.rb
@@ -9,7 +9,7 @@ module Calculate
                 puts ENDER
                 puts equation.to_s + " = " + solved_equation.to_s
             rescue SyntaxError => se
-                puts "Wrong Equation Provided"
+                puts "Wrong Equation Provided..."
             end
         end
     end
